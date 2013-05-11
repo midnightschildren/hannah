f90ce96cc5de43346b37c820c3caebdb4f4d82e0
midnightschildren <amar@codisattva.com>
2013-05-10 22:46:34 -0700

    Initial Commit of Responsive Grids

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<script type="text/javascript" src="//use.typekit.net/tlm7ich.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 <title>Hannah Sowd | Skin Care & Massage | San Francisco California</title>
 
@@ -17,6 +19,7 @@
 <meta name="viewport" content="width=device-width">
 
 <link rel="stylesheet" href="template/css/style.css">
+<link rel="stylesheet" href="template/css/grid.css">
 
 <script src="js/libs/modernizr-2.5.3.min.js"></script>
 
@@ -47,7 +50,7 @@
 
 
 <h2 class="slabtextdone"><span class="slabtext">415.830.4227</span></h2>
-<h2 class="slabtextdone"><span class="slabtext">hannahsowd@gmail &middot; com</span></h2>
+<h2 class="slabtextdone"><span class="slabtext">h a n n a h s o w d @ g m a i l &middot; c o m</span></h2>
 <h2 class="slabtextdone"><span class="slabtext">1640 Bush Street</span></h2>
 <h2 class="slabtextdone"><span class="slabtext">San Francisco &middot; California &middot; 94109</span></h2>
 
@@ -67,22 +70,57 @@
   
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="js/libs/jquery.slabtext.min.js"></script>
-  <script>
-    // Function to slabtext the H2 headings
-    
+<script>
+  
+  // smart resize - http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
+  (function($,sr){
+ 
+    // debouncing function from John Hann
+    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+    var debounce = function (func, threshold, execAsap) {
+        var timeout;
+   
+        return function debounced () {
+            var obj = this, args = arguments;
+            function delayed () {
+                if (!execAsap)
+                    func.apply(obj, args);
+                timeout = null; 
+            };
+   
+            if (timeout)
+                clearTimeout(timeout);
+            else if (execAsap)
+                func.apply(obj, args);
+   
+            timeout = setTimeout(delayed, threshold || 100); 
+        };
+    }
+    // smartresize 
+    jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+   
+  })(jQuery,'smartresize');
+
+</script>
+<script>
+    // Function to slabtext the H1 headings
+    function slabTextHeadlines() {
         $("h2").slabText({
             // Don't slabtext the headers if the viewport is under 380px
             "viewportBreakpoint":380
         });
-    
+    };
     
     // Called one second after the onload event for the demo (as I'm hacking the
     // fontface load event a bit here)
 
     // Please do not do this in a production environment - you should really use
     // google WebFont loader events (or something similar) for better control
-   
-  </script>
+    $(window).load(function() {
+        // So, to recap... don't actually do this, it's nasty!
+        setTimeout(slabTextHeadlines, 1000);
+    });
+  </script>  
 
 <script src="js/plugins.js"></script>
 <script src="js/script.js"></script>
