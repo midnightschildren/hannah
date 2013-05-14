6b11501b58aee526067bd1d030a75dffdd898bb1
midnightschildren <amar@codisattva.com>
2013-05-14 00:08:43 -0700

    Services page & retina font changes + responsive image

--- services.php
+++ services.php
@@ -2,29 +2,35 @@
 
 <!-- Navigation, IDs are used to create certain widths of the <a> tag's. Designers choice not to use a font that has not been created into a web font yet. Thus this page being produced with a bunch of back ground images -->
 
-	<nav>
-	<ul>
-	<li><a href="about-hannah.php" id="nav-about">WHO IS HANNAH</a></li>
-	<li><a href="http://www.genbook.com/bookings/slot/reservation/30051977?bookingSourceId=1000" target="_blank" id="nav-shedule">SCHEDULE/BOOK</a></li>
-	<li><a href="services.php" id="active-nav-services">SERVICES</a></li>
-	<li><a href="http://blog.hannahsowd.com" target="_blank" id="nav-blog">READ HER BLOG</a></li>
-	<li><a href="http://shop.hannahsowd.com/" target="_blank" id="nav-shop">SHOP</a></li>
-	</ul>
-	</nav>
+<nav>
+<ul>
+
+<div class="grid-2 m-grid-1 s-hidden">&nbsp;</div>
+<div class="grid-8 m-grid-10 s-grid-12 s-padded-sides">
+<li class="grid-3"><a href="about-hannah.php" id="nav-about">WHO IS HANNAH</a></li>
+<li class="grid-3"><div class="centerblock"><a href="http://www.genbook.com/bookings/slot/reservation/30051977?bookingSourceId=1000" target="_blank" id="nav-shedule">SCHEDULE/BOOK</a></div></li>
+<li class="grid-2"><div class="centerblock"><a href="services.php" id="active-nav-services">SERVICES</a></div></li>
+<li class="grid-3"><div class="centerblock"><a href="http://blog.hannahsowd.com" target="_blank" id="nav-blog">READ HER BLOG</a></div></li>
+<li class="grid-1"><a href="http://shop.hannahsowd.com/" target="_blank" id="nav-shop">SHOP</a></li>
+	
+</div>
+<div class="grid-2 m-grid-1 s-hidden">&nbsp;</div>
+</ul>
+</nav>
 
   
-<div id="wrap">
+<div id="wraps" class="grid-whole">
 
 <!-- left Content -->
-
-<div class="column" id="left-column">
+<div class="grid-7 s-grid-12 padded-righter">
+<div class="column grid-whole spc" style="padding-bottom: 30px; padding-top: 37px;" id="left-column">
 
 <?php include("pulsepro/data/blocks/Services/Services-List.html"); ?>
 
 </div>
-
+</div>
 <!-- Right Content -->
-
+<div class="grid-5 s-grid-12 padded-lefter">
 <div class="column" id="right-column">
 
 <?php include("pulsepro/data/blocks/Services/Hours-of-Business.html"); ?>
@@ -32,5 +38,5 @@
 <?php include("pulsepro/data/blocks/Services/Types-of-Payment.html"); ?>
 
 </div>
-
+</div>
 <?php include_once("template/footer.php"); ?>
